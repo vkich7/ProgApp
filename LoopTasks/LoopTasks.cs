2d8f50fee08b00fa331bfef61f6ce803f85e7f40
Vasyl Kichula <104771020+vkich7@users.noreply.github.com>
2024-02-19 08:34:54 +0200

    St

---
+++ LoopTasks/LoopTasks.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace LoopTasks
+{
+    public static class LoopTasks
+    {
+        /// <summary>
+        /// Task 1
+        /// </summary>
+        public static int SumOfOddDigits(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Task 2
+        /// </summary>
+        public static int NumberOfUnitsInBinaryRecord(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Task 3 
+        /// </summary>
+        public static int SumOfFirstNFibonacciNumbers(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
